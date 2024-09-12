ac706e6fb5068c219c193a6e68c8d94fb474aa84
Lester Maranan <marklestermarananpal@gmail.com>
2024-09-12 09:47:07 +0800

    back to public

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -34,51 +34,51 @@ class FileController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-  
-     public function store(Request $request)
-     {
-         $senderId = Auth::id();
-         $input = $request->all();
-     
-         // Get the file from the request
-         $file = $request->file('file');
-     
-         // Get the file name and extension
-         $filename = $request->input('file_name');
-         $fileExtension = $file->getClientOriginalExtension();
-         $filename = $filename . '.' . $fileExtension;
-     
-         // Store the file on the FTP server
-         $filePath = Storage::disk('ftp')->putFileAs('files', $file, $filename);
-     
-         // Extract category from request
-         $category = $request->input('category');
-     
-         // Retrieve the last document based on the category
-         $lastDocument = Document::where('category', $category)
-             ->latest('updated_at')
-             ->whereNotNull('code')
-             ->first();
-     
-         $lastCode = null;
-         if ($lastDocument) {
-             $lastCode = $lastDocument->code;
-         }
-     
-         // Update the input array with additional data
-         $input['file'] = $filename;
-         $input['sender_id'] = $senderId;
-         $input['status'] = 'pending';
-         $input['sended_date'] = now();
-         $input['recieved_by'] = 2;
-         $input['type'] = 'incoming';
-         $input['lastcode'] = $lastCode;
-     
-         // Create a new document record
-         Document::create($input);
-     
-         return redirect('/user-outgoing')->with('success', 'Your file outgoing is successful.');
-     }
+    public function store(Request $request)
+    {
+        $senderId = Auth::id();
+        $input = $request->all();
+        
+        // Get the file from the request
+        $file = $request->file('file');
+        
+        // Get the file name and extension
+        $filename = $request->input('file_name');
+        $fileExtension = $file->getClientOriginalExtension();
+        $filename = $filename . '.' . $fileExtension;
+        
+        // Store the file in the public directory
+        $filePath = Storage::disk('public')->putFileAs('files', $file, $filename);
+        
+        // Extract category from request
+        $category = $request->input('category');
+        
+        // Retrieve the last document based on the category
+        $lastDocument = Document::where('category', $category)
+            ->latest('updated_at')
+            ->whereNotNull('code')
+            ->first();
+        
+        $lastCode = null;
+        if ($lastDocument) {
+            $lastCode = $lastDocument->code;
+        }
+        
+        // Update the input array with additional data
+        $input['file'] = $filename;
+        $input['sender_id'] = $senderId;
+        $input['status'] = 'pending';
+        $input['sended_date'] = now();
+        $input['recieved_by'] = 2;
+        $input['type'] = 'incoming';
+        $input['lastcode'] = $lastCode;
+        
+        // Create a new document record
+        Document::create($input);
+        
+        return redirect('/user-outgoing')->with('success', 'Your file outgoing is successful.');
+    }
+    
      
     /**
      * Display the specified resource.
